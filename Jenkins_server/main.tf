602e0a6ec8084f2a065d435c6ec1e19c0d57b808
Devops-13999 <dev377627@gmail.com>
2024-09-04 00:30:38 +0530

    commit jenkinsfile

--- Jenkins_server/main.tf
+++ Jenkins_server/main.tf
@@ -16,7 +16,7 @@ module "EC2_Module" {
   user_data = <<-EOF
   #! /bin/bash
   sudo apt update
-  sudo apt install -y openjdk-11-jdk
+  sudo apt install openjdk-17-jdk -y
   
   curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
